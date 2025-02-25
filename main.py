50cd9d24f6d2401b6e37eadf256ed834895d0d35
madhuvamsi <madhuvamsi135@gmail.com>
2025-02-25 20:57:27 +0530

    model evaluation done

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.textSummarizer.pipeline.stage_2_data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.textSummarizer.pipeline.stage_3_model_trainer_pipeline import ModelTrainerTrainingPipeline
-
+from src.textSummarizer.pipeline.stage_4_model_evaluation_pipeline import ModelEvaluationTrainingPipeline
 STAGE_NAME = "Data Ingestion stage"
 
 try:
@@ -35,6 +35,18 @@ try:
     logger.info(f"stage {STAGE_NAME} initiated")
     model_trainer_pipeline = ModelTrainerTrainingPipeline()
     model_trainer_pipeline.initiate_model_trainer()
+    logger.info(f"stage {STAGE_NAME} completed")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model evaluation stage"
+
+try:
+    logger.info(f"stage {STAGE_NAME} initiated")
+    model_evaluation_pipeline = ModelEvaluationTrainingPipeline()
+    model_evaluation_pipeline.initiate_model_evaluation()
     logger.info(f"stage {STAGE_NAME} completed")
 
 except Exception as e:
